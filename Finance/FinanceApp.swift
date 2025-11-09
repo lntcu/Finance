bc34e83e3fd7a669a346df70f97ef39d43d17f6f
lntcu <221248485+lntcu@users.noreply.github.com>
2025-11-09 08:53:45 +0800

    Initial Commit

---
+++ Finance/FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceApp.swift
+//  Finance
+//
+//  Created by user on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
